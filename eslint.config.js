52800b66267b0b045bc2aaa3b025e3937cce4f3d
gbavieira <gabrielattie@gmail.com>
2026-04-11 22:32:46 -0300

    feat: Adding Linter

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+export default [
+  {
+    ignores: ['node_modules/**', 'packs/**', 'dist/**']
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: 'module',
+      globals: {
+        // Foundry VTT globals
+        game: 'readonly',
+        ui: 'readonly',
+        canvas: 'readonly',
+        CONFIG: 'readonly',
+        Actors: 'readonly',
+        Items: 'readonly',
+        ChatMessage: 'readonly',
+        ActiveEffect: 'readonly',
+        Token: 'readonly',
+        MeasuredTemplate: 'readonly',
+        Dialog: 'readonly',
+        Hooks: 'readonly',
+        foundry: 'readonly',
+        renderTemplate: 'readonly',
+        Sequence: 'readonly',
+        FilePicker: 'readonly',
+        TextEditor: 'readonly',
+        Roll: 'readonly',
+        DocumentSheetV2: 'readonly',
+        ApplicationV2: 'readonly'
+      }
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'eqeqeq': ['error', 'always'],
+      'no-var': 'error',
+      'prefer-const': 'error'
+    }
+  }
+];
